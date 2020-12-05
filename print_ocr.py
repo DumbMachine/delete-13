d60a8a6176f8753ed70ec584d1dc4f5958ab9bed
Ratin <ratin.kumar.2k@gmail.com>
2020-12-06 00:15:20 +0530

    temp

--- print_ocr.py
+++ print_ocr.py
@@ -3,25 +3,28 @@
 # offers visual insight into the dataset
 import numpy as np
 
+
 def print_letter(letter):
-	for i in range (0, 16):
-		for j in range(0, 8):
-			print("00" if (letter[(8*i + j)] == "1") else "  ", end=""),
-		print ()
+    for i in range(0, 16):
+        for j in range(0, 8):
+            print("00" if (letter[(8 * i + j)] == "1") else "  ", end=""),
+        print()
+
 
 def print_data(label, image):
-	print("============================")
-	print('label: ')
-	print(label)
-	print("----------------------------")
-	print_letter(image)
-	print("============================")
-	print('\n\n\n\n\n\n')
+    print("============================")
+    print("label: ")
+    print(label)
+    print("----------------------------")
+    print_letter(image)
+    print("============================")
+    print("\n\n\n\n\n\n")
 
-data = np.load('letters.npz')
 
-images = data['images']
-labels = data['labels']
+data = np.load("letters.npz")
+
+images = data["images"]
+labels = data["labels"]
 
 for i in range(len(images)):
-	print_data(labels[i], images[i])
+    print_data(labels[i], images[i])
--- process_data.py
+++ process_data.py
@@ -4,25 +4,24 @@ import csv
 import numpy as np
 import string
 
+
 def string_vectorizer(strng, alphabet=string.ascii_lowercase):
-    vector = [[0 if char != letter else 1 for char in alphabet] 
-                  for letter in strng]
+    vector = [[0 if char != letter else 1 for char in alphabet] for letter in strng]
     return vector[0]
 
+
 def extract_data(x):
-	return {
-		'label': string_vectorizer(x[1]),
-		'images': x[6:134]
-	}
+    return {"label": string_vectorizer(x[1]), "images": x[6:134]}
+
 
 images = []
 labels = []
 
-with open('letter.data') as file:
-	reader = csv.reader(file, delimiter='\t')
-	for row in reader:
-		data = extract_data(row)
-		images.append(data['images'])
-		labels.append(data['label'])
+with open("letter.data") as file:
+    reader = csv.reader(file, delimiter="\t")
+    for row in reader:
+        data = extract_data(row)
+        images.append(data["images"])
+        labels.append(data["label"])
 
 np.savez("letters", images=images, labels=labels)
